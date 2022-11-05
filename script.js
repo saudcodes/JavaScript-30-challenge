3fe790dab3b51c493325759dd53607b47aa69805
Saudcodes <saudstunts@gmail.com>
2022-11-05 15:33:25 +0400

    create function

---
+++ script.js
@@ -0,0 +1,17 @@
+window.addEventListener("keydown", (e) => {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+    if (!audio) return;
+    audio.currentTime = 1
+    console.log(audio)
+    key.classList.add("playing")
+    audio.play()
+})
+
+let removetransition = (e) => {
+    if (e.propertyName != "transform") return;
+    this.remove.classList("playing")
+    console.log("true")
+}
+let keys = document.querySelectorall(".key")
+keys.forEach(key => key.addEventListener("transitionend", removetransition));
