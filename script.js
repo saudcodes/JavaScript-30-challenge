2695361360fd4e042568cc9b9827499836cf6d59
Saudcodes <saudstunts@gmail.com>
2022-11-06 10:07:02 +0400

    JavaScript Day 1

--- script.js
+++ script.js
@@ -5,19 +5,21 @@ let playMusic = (e) => {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
     if (!audio) return;
-    audio.currentTime = 1
+    audio.currentTime = 0;
     console.log(audio)
     key.classList.add("playing")
     audio.play()
+    console.log(keys)
 }
 
 let removetransition = (e) => {
-    if (e.propertyName != "transform") return;
-    this.remove.classList("playing")
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');
     console.log("true")
 }
-let keys = document.querySelectorall(".key")
-keys.forEach(key => key.addEventListener("transitionend", removetransition));
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('keyup', removetransition));
 window.addEventListener("keydown", playMusic
 
 )
+
